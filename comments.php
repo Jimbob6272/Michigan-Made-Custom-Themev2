6ce336f1240bdb2f1f9141a11cec372cbc2a754c
Jim Lheureux <lheureux.james@gmail.com>
2020-08-06 23:49:28 -0400

    first commit

---
+++ comments.php
@@ -0,0 +1,14 @@
+<?php
+/*
+The comments page for Bones
+*/
+
+// don't load it if you can't comment
+if ( post_password_required() ) {
+  return;
+}
+
+?>
+
+<?php // You can start editing here. ?>
+
